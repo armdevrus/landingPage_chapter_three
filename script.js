aa25b4c0944bf8450ddfb4e5e6e5641e9d1a322c
Arman <aisenovdev@gmail.com>
2022-02-17 15:29:18 +0500

    timer has changed, it's dynamic

--- script.js
+++ script.js
@@ -32,4 +32,49 @@ window.addEventListener('DOMContentLoaded', function () {
       }
     }
   });
+
+  // Timer
+
+  let deadline = '2022-02-18';
+  
+  function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+      seconds = Math.floor((t / 1000) % 60),
+      minutes = Math.floor((t / 1000 / 60) % 60),
+      hours = Math.floor(t / (1000 * 60 * 60));
+
+    return {
+      'total': t,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'), 
+      timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endtime);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
+      } else {
+        t.hours >= 10 ? hours.textContent = t.hours : hours.textContent = '0' + t.hours;
+        t.minutes >= 10 ? minutes.textContent = t.minutes : minutes.textContent = '0' + t.minutes;
+        t.seconds >= 10 ? seconds.textContent = t.seconds : seconds.textContent = '0' + t.seconds;
+      }
+    }
+  }
+
+  setClock('timer', deadline);
+
 });
+
