7666ee6397a151e4619395233aff2441c2c6c49b
Arman <aisenovdev@gmail.com>
2022-03-03 18:48:59 +0500

    works sending data of user from form

--- script.js
+++ script.js
@@ -1,106 +1,153 @@
 window.addEventListener('DOMContentLoaded', function () {
-  'use stirct';
-  let tabHeader = document.querySelectorAll('.info-header-tab'),
-    tabHeaderParent = document.querySelector('.info-header'),
-    tabContent = document.querySelectorAll('.info-tabcontent');
+    'use stirct';
+    let tabHeader = document.querySelectorAll('.info-header-tab'),
+        tabHeaderParent = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
 
-  function hideTabContent(a) {
-    for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove('show'),
-        tabContent[i].classList.add('hide');
-    }
-  }
-
-  hideTabContent(1);
-
-  function showTabContent(b) {
-    if (tabContent[b].classList.contains('hide')) {
-      tabContent[b].classList.remove('hide'),
-        tabContent[b].classList.add('show');
-    }
-  }
-
-  tabHeaderParent.addEventListener('click', function (event) {
-    let target = event.target;
-    if (target && target.classList.contains('info-header-tab')) {
-      for (let i = 0; i < tabHeader.length; i++) {
-        if (target == tabHeader[i]) {
-          hideTabContent(0);
-          showTabContent(i);
-          break;
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show'),
+                tabContent[i].classList.add('hide');
         }
-      }
     }
-  });
 
-  // Timer
+    hideTabContent(1);
 
-  let deadline = '2022-03-18';
-
-  function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date()),
-      seconds = Math.floor((t / 1000) % 60),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      hours = Math.floor(t / (1000 * 60 * 60));
-
-    return {
-      'total': t,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
-  function setClock(id, endtime) {
-    let timer = document.getElementById(id),
-      hours = timer.querySelector('.hours'),
-      minutes = timer.querySelector('.minutes'),
-      seconds = timer.querySelector('.seconds'),
-      timeInterval = setInterval(updateClock, 1000);
-
-    function updateClock() {
-      let t = getTimeRemaining(endtime);
-
-      if (t.total <= 0) {
-        clearInterval(timeInterval);
-        hours.textContent = '00';
-        minutes.textContent = '00';
-        seconds.textContent = '00';
-      } else {
-        t.hours >= 10 ? hours.textContent = t.hours : hours.textContent = '0' + t.hours;
-        t.minutes >= 10 ? minutes.textContent = t.minutes : minutes.textContent = '0' + t.minutes;
-        t.seconds >= 10 ? seconds.textContent = t.seconds : seconds.textContent = '0' + t.seconds;
-      }
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide'),
+                tabContent[b].classList.add('show');
+        }
     }
-  }
 
-  setClock('timer', deadline);
-
-  // Modal window
-
-  let more = document.querySelector('.more'),
-    overlay = document.querySelector('.overlay'),
-    close = document.querySelector('.popup-close'),
-    descriptionBtn = document.querySelectorAll('.description-btn');
-
-  more.addEventListener('click', function () {
-    overlay.style.display = 'block';
-    this.classList.add('more-splash');
-    document.body.style.overflow = 'hidden';
-  });
-
-  close.addEventListener('click', function () {
-    overlay.style.display = 'none';
-    more.classList.remove('more-splash');
-    document.body.style.overflow = '';
-  });
-
-  descriptionBtn.forEach(function(item) {
-    item.addEventListener('click', function(){
-      overlay.style.display = 'block';
-      more.classList.add('more-splash');
-      document.body.style.overflow = 'hidden';
+    tabHeaderParent.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tabHeader.length; i++) {
+                if (target == tabHeader[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
     });
-  });
-});
 
+    // Timer
+
+    let deadline = '2022-03-18';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / (1000 * 60 * 60));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            } else {
+                t.hours >= 10 ? hours.textContent = t.hours : hours.textContent = '0' + t.hours;
+                t.minutes >= 10 ? minutes.textContent = t.minutes : minutes.textContent = '0' + t.minutes;
+                t.seconds >= 10 ? seconds.textContent = t.seconds : seconds.textContent = '0' + t.seconds;
+            }
+        }
+    }
+
+    setClock('timer', deadline);
+
+    // Modal window
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descriptionBtn = document.querySelectorAll('.description-btn');
+
+    more.addEventListener('click', function () {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function () {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    descriptionBtn.forEach(function (item) {
+        item.addEventListener('click', function () {
+            overlay.style.display = 'block';
+            more.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');  
+
+            let formData = new FormData(form);
+
+            let obj = {};
+            formData.forEach(function(value, key){
+                obj[key] = value;
+            })
+            
+            let json = JSON.stringify(obj);
+
+
+        request.send(json);
+
+            request.addEventListener('readystatechange', function(){
+                if(request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState == 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            })
+            for(let i = 0; i < input.length; i++){
+                input[i].value = '';
+            }
+    })
+});
