95ad0a05c5969c1e960c1812186fe0ea9ca6f3e5
Arman <aisenovdev@gmail.com>
2022-03-05 19:40:15 +0500

    added promise in form

--- script.js
+++ script.js
@@ -115,39 +115,49 @@ window.addEventListener('DOMContentLoaded', function () {
         input = form.getElementsByTagName('input'),
         statusMessage = document.createElement('div');
 
-        statusMessage.classList.add('status');
+    statusMessage.classList.add('status');
 
-    form.addEventListener('submit', function(event){
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
         form.appendChild(statusMessage);
 
         let request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');  
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
 
-            let formData = new FormData(form);
+        let formData = new FormData(form);
 
-            let obj = {};
-            formData.forEach(function(value, key){
-                obj[key] = value;
-            })
-            
-            let json = JSON.stringify(obj);
+        let obj = {};
+        formData.forEach(function (value, key) {
+            obj[key] = value;
+        })
 
+        let json = JSON.stringify(obj);
 
         request.send(json);
 
-            request.addEventListener('readystatechange', function(){
-                if(request.readyState < 4) {
-                    statusMessage.innerHTML = message.loading;
-                } else if (request.readyState == 4 && request.status == 200) {
-                    statusMessage.innerHTML = message.success;
-                } else {
-                    statusMessage.innerHTML = message.failure;
-                }
-            })
-            for(let i = 0; i < input.length; i++){
-                input[i].value = '';
+        request.addEventListener('readystatechange', function () {
+            function postData() {
+                return new Promise(function (resolve, reject) {
+                    if (request.readyState < 4) {
+                        resolve()
+                    } else if (request.readyState == 4 && request.status == 200) {
+                        resolve()
+                    } else {
+                        reject()
+                    }
+                })
             }
+            function clearInput(){
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+                }
+            }
+            postData()
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => statusMessage.innerHTML = message.success)
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput)
+        })
     })
 });
