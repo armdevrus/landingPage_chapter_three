8ce08e443778c540c696a63b5df41a9303f260bd
Arman <aisenovdev@gmail.com>
2022-03-06 20:23:21 +0500

    calculator works

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ window.addEventListener('DOMContentLoaded', function () {
                     }
                 })
             }
-            function clearInput(){
+            function clearInput() {
                 for (let i = 0; i < input.length; i++) {
                     input[i].value = '';
                 }
@@ -160,4 +160,105 @@ window.addEventListener('DOMContentLoaded', function () {
                 .then(clearInput)
         })
     })
+
+    // Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex)
+    function showSlides(i) {
+
+        if (i > slides.length) {
+            slideIndex = 1;
+        }
+        if (i < 1) {
+            slideIndex = slides.length
+        }
+
+        slides.forEach(function (item) {
+            item.style.display = 'none';
+        });
+        // for(let i = 0; i < slides.length; i++){
+        //     slides[i].style.display = 'none';
+        // }
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlide(i) {
+        showSlides(slideIndex += i);
+    }
+    function currentSlide(i) {
+        showSlides(slideIndex = i);
+    }
+
+    prev.addEventListener('click', function () {
+        plusSlide(-1);
+    });
+
+    next.addEventListener('click', function () {
+        plusSlide(1);
+    });
+
+    dotsWrap.addEventListener('click', function (event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+                currentSlide(i)
+            }
+        }
+    });
+
+    // Calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+    totalValue.textContent = total;
+
+   
+
+    persons.addEventListener('change', function () {
+        personsSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (restDays.value == '' || this.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    restDays.addEventListener('change', function () {
+        daysSum = +this.value;
+        total = (daysSum + personsSum) * 4000;
+
+        if (persons.value == '' || this.value == '') {
+            totalValue.textContent = 0;
+        } else {
+            totalValue.textContent = total;
+        }
+    });
+
+    place.addEventListener('change', function(){
+        if (persons.value == '' || restDays.value == ''){
+            totalValue.textContent = 0
+        } else {
+            let a = total;
+            totalValue.textContent = a * this[this.selectedIndex].value;
+        }
+    })
+   
 });
